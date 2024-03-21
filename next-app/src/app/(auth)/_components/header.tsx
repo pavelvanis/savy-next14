3db341196f4e8584820f3df8ac9b1c76d2dc7629
vanis <pavlik.vanis@email.cz>
2024-03-21 12:07:41 +0100

    Add AuthHeader component

---
+++ next-app/src/app/(auth)/_components/header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+const AuthHeader = ({ className }: PropsWithClassName) => {
+  return (
+    <header
+      className={cn("h-14 flex justify-between align-center px-8", className)}
+    >
+      <div className="flex justify-center items-center">logo</div>
+      <nav className="flex" >
+        <ul className="flex justify-between items-center gap-x-10 ">
+          <li>docs</li>
+          <li>about</li>
+          <li>security</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default AuthHeader;
