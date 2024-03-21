3dad60264d70c0d68fd1e7b91785eb01029cac18
vanis <pavlik.vanis@email.cz>
2024-03-21 13:39:56 +0100

    Refactor footer component in AuthFooter

--- next-app/src/app/(auth)/_components/footer.tsx
+++ next-app/src/app/(auth)/_components/footer.tsx
@@ -2,7 +2,28 @@ import React from "react";
 import { cn } from "@/lib/utils";
 
 const AuthFooter = ({ className }: PropsWithClassName) => {
-  return <div className={cn(className)}>Footer</div>;
+  return (
+    <footer className={cn("flex justify-between py-5 px-8", className)}>
+      <div className="flex flex-col justify-between">
+        <div>Min logo</div>
+        <div className="font-light text-xs">Copyright © Pavel Vanis</div>
+      </div>
+      <div className="flex gap-10">
+        <ul>
+          <li>link</li>
+          <li>link</li>
+          <li>link</li>
+          <li>link</li>
+        </ul>
+        <ul>
+          <li>link</li>
+          <li>link</li>
+          <li>link</li>
+          <li>link</li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default AuthFooter;
