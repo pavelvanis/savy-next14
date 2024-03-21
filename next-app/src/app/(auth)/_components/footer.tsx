62080c40a63feb85293cb64c2dfe89066c7de357
vanis <pavlik.vanis@email.cz>
2024-03-21 12:07:46 +0100

    Add AuthFooter component

---
+++ next-app/src/app/(auth)/_components/footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+const AuthFooter = ({ className }: PropsWithClassName) => {
+  return <div className={cn(className)}>Footer</div>;
+};
+
+export default AuthFooter;
