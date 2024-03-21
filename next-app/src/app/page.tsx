daaa0ce00a7c3031971c102a8da33ed58be021cd
vanis <pavlik.vanis@email.cz>
2024-03-21 09:58:23 +0100

    Use TinkLink testing button

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,8 +1,10 @@
+import TinkLink from "@/components/test/tink-link";
 
 export default function Home() {
   return (
     <main>
       <h1>savy-next14</h1>
+      <TinkLink />
     </main>
   );
 }
