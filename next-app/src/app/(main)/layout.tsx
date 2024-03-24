a3ff45bb55861acaadb4f2d536cbb9aa2e29da42
vanis <pavlik.vanis@email.cz>
2024-03-24 18:31:39 +0100

    Add header and footer components to MainLayout

--- next-app/src/app/(main)/layout.tsx
+++ next-app/src/app/(main)/layout.tsx
@@ -1,7 +1,15 @@
 import React, { PropsWithChildren } from "react";
+import MainHeader from "./_components/header";
+import MainFooter from "./_components/footer";
 
 const MainLayout = ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
+  return (
+    <>
+      <MainHeader className="" />
+      <main className="">{children}</main>
+      <MainFooter className="" />
+    </>
+  );
 };
 
 export default MainLayout;
