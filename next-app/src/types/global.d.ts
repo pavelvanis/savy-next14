7dc1df1f97972674e7c7912a0bf6f37194b110e4
vanis <pavlik.vanis@email.cz>
2024-03-24 18:28:48 +0100

    Update MONGO_URI to MONGODB_URI

--- next-app/src/types/global.d.ts
+++ next-app/src/types/global.d.ts
@@ -2,7 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       // Mongo database
-      MONGO_URI: string;
+      MONGODB_URI: string;
       // Tink API
       TINK_CLIENT_ID: string;
       TINK_CLIENT_SECRET: string;
