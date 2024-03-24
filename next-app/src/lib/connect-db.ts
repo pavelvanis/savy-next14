475575fe6a5841eba7733fc102536484ec263721
vanis <pavlik.vanis@email.cz>
2024-03-24 18:29:25 +0100

    Create connectDB function to handle MongoDB connection

---
+++ next-app/src/lib/connect-db.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+export const connectDB = async () => {
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local"
+    );
+  }
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  return mongoose.connect(MONGODB_URI);
+};
