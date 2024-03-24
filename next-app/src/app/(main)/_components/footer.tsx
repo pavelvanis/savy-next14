760c5608ed70f08f8792e93cf3a00b321d6c074e
vanis <pavlik.vanis@email.cz>
2024-03-24 18:31:24 +0100

    Add MainFooter component

---
+++ next-app/src/app/(main)/_components/footer.tsx
@@ -0,0 +1,7 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+const MainFooter = ({ className }: PropsWithClassName) => {
+  return <div className={cn(className)}>footer</div>;
+};
+export default MainFooter;
