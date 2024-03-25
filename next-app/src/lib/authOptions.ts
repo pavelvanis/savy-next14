5b255561ae76a3d9a0f1d86657a4becd279f856e
vanis <pavlik.vanis@email.cz>
2024-03-25 22:10:01 +0100

    Add JWT max age and CSRF token to authOptions

--- next-app/src/lib/authOptions.ts
+++ next-app/src/lib/authOptions.ts
@@ -1,20 +1,36 @@
-import { NextAuthOptions, User } from "next-auth";
+import { getServerSession, NextAuthOptions, User } from "next-auth";
 import CredentialProvider from "next-auth/providers/credentials";
+import { getCsrfToken } from "next-auth/react";
+import { LocalApiAxios } from "./axios";
 
 const authOptions: NextAuthOptions = {
+  // Setup max age to 24 hours
+  jwt: {
+    maxAge: 60 * 60 * 24,
+  },
   callbacks: {
     async jwt({ token, user, account, profile }) {
       // console.log("jwt callback", { token, user, account, profile });
+
+      console.log("JWT was called...");
+
+      // Calls when user logged in
       if (user) {
-        return { user: user };
+        const csrf = await getCsrfToken();
+        return { user: user, csrfToken: csrf };
       }
       return token;
     },
     async session({ session, token }) {
       // console.log("session callback", { session, token });
-      const user = token.user as { name: string };
 
-      return { ...session, user: { ...user } };
+      console.log("Session was called...");
+
+      // Add user and csrf token to session
+      const user = token.user as User;
+      const csrf = token.csrfToken as string;
+
+      return { ...session, user: { ...user }, csrfToken: csrf };
     },
   },
   providers: [
@@ -24,13 +40,15 @@ const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       authorize: async (credentials) => {
-        const user = {
-          id: "123",
-          name: "John Smith",
-          email: "john.smith@example.com",
-          user_id: "123",
-          user_ID: "123",
-        };
+        console.log(credentials);
+        const login = await LocalApiAxios.post("/auth/login", credentials);
+
+        if (!login) {
+          return null;
+        }
+
+        const user = login.data as User;
+
         return user;
       },
     }),
