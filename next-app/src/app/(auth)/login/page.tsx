38b31d643aebcf908fec230693a54dd29ed1cddc
vanis <pavlik.vanis@email.cz>
2024-03-27 11:52:34 +0100

    Add LoginForm component to LoginPage

--- next-app/src/app/(auth)/login/page.tsx
+++ next-app/src/app/(auth)/login/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import LoginForm from "@/components/auth/login-form";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <LoginForm />;
 };
 
 export default LoginPage;
