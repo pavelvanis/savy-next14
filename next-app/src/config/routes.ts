eaae6100cc302c70a124b94e4fe17d308108b31b
vanis <pavlik.vanis@email.cz>
2024-03-28 23:44:52 +0100

    Add authNavLinks to routes.ts

--- next-app/src/config/routes.ts
+++ next-app/src/config/routes.ts
@@ -1,4 +1,14 @@
+import { NavLinkProps } from "@/components/navlink";
+
 // Routes will be inserted into the NextAuth middleware
 export const NEXTAUTH_ROUTES: string[] = ["/dashboard", "/admin", "/settings"];
 
+// Default redirect after login
 export const DEFAULT_LOGIN_REDIRECT = "/";
+
+// Links for haeder in auth layout
+export const authNavLinks: NavLinkProps[] = [
+  { href: "/", title: "Home" },
+  { href: "#", title: "Security" },
+  { href: "#", title: "Privacy" },
+];
