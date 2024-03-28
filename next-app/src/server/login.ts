bbf5794b74bfd372826219e61e7fcb69594d5f3d
vanis <pavlik.vanis@email.cz>
2024-03-28 16:03:17 +0100

    Update login function to use client-side validation and handle response errors

--- next-app/src/server/login.ts
+++ next-app/src/server/login.ts
@@ -1,48 +1,29 @@
-"use server";
+"use client";
 
 import * as z from "zod";
 import { signIn } from "next-auth/react";
-
 import { LoginSchema } from "@/schemas";
-import { connectDB } from "@/lib/connect-db";
-import { UserModel } from "@/database/models";
-import { DEFAULT_LOGIN_REDIRECT } from "@/config/routes";
+import { loginValidation } from "./login-validation";
 
-/**
- * Asynchronously logs in a user.
- *
- * @param {Object} values - The user's login information. The object structure is inferred from the `LoginSchema`.
- * @param {string} [callbackUrl=null] - A URL to redirect to after successful login. If not provided, the user is redirected to the `DEFAULT_LOGIN_REDIRECT` URL.
- *
- * @returns {Promise<Object>} If the validation fails, it returns a promise that resolves to an object with a `errors` property, which is an object containing the validation errors.
- * If the user does not exist, it returns a promise that resolves to an object with a `message` property with the value "User does not exist!".
- * If the login is successful, it does not return anything.
- */
-export const login = async (
-  values: z.infer<typeof LoginSchema>,
-  callbackUrl?: string | null
-) => {
-  const validatedFields = LoginSchema.safeParse(values);
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  try {
+    const validation = await loginValidation(values);
 
-  if (!validatedFields.success) {
-    return { error: "Invalid fields!" };
+    if (validation?.error) {
+      return validation;
+    }
+
+    const response = await signIn("credentials", {
+      ...values,
+      redirect: false,
+    });
+
+    if (!response?.ok) {
+      return { error: "Invalid password" };
+    }
+
+    return { success: "You have been logged in!" };
+  } catch (error) {
+    return { error: "An error occurred. Please try again." };
   }
-
-  await connectDB();
-
-  const { email, password } = validatedFields.data;
-
-  const existingUser = await UserModel.findOne({
-    email,
-  });
-
-  if (!existingUser || !existingUser.password) {
-    return { error: "User does not exist!" };
-  }
-
-  return await signIn("credentials", {
-    email,
-    password,
-    redirectTo: callbackUrl || DEFAULT_LOGIN_REDIRECT,
-  });
 };
