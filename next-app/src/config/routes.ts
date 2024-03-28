480f3d07912c5a2017f3a431b8ab9097bddd4d35
vanis <pavlik.vanis@email.cz>
2024-03-28 16:07:03 +0100

    Add `DEFAULT_LOGIN_REDIRECT` constant to routes.ts

--- next-app/src/config/routes.ts
+++ next-app/src/config/routes.ts
@@ -1,2 +1,4 @@
 // Routes will be inserted into the NextAuth middleware
 export const NEXTAUTH_ROUTES: string[] = ["/dashboard", "/admin", "/settings"];
+
+export const DEFAULT_LOGIN_REDIRECT = "/";
