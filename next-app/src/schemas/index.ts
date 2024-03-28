7d74d77a14d4ae733166438a03a89185351a4e86
vanis <pavlik.vanis@email.cz>
2024-03-28 11:21:57 +0100

    Refactor RegisterSchema in index.ts

--- next-app/src/schemas/index.ts
+++ next-app/src/schemas/index.ts
@@ -12,15 +12,31 @@ export const LoginSchema = z.object({
     .max(100, { message: "Password is too long" }),
 });
 
-export const RegisterSchema = z.object({
-  name: z.string().min(2, { message: "Name is too small" }),
-  email: z.string().email({
-    message: "Email is required",
-  }),
-  password: z
-    .string()
-    .min(4, {
-      message: "Password is too small",
-    })
-    .max(100, { message: "Password is too long" }),
-});
+export const RegisterSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(2, { message: "First name is too small" })
+      .max(100, "First name is too long"),
+    lastName: z
+      .string()
+      .min(2, { message: "Last name is too small" })
+      .max(100, "Last name is too long"),
+    email: z.string().email({
+      message: "Email is required",
+    }),
+    password: z
+      .string()
+      .min(4, {
+        message: "Password is too small",
+      })
+      .max(100, { message: "Password is too long" }),
+    confirmPassword: z
+      .string()
+      .min(4, "You must confirm your password")
+      .max(100, "Password is too long"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
