7f0a835c00dc0ca906b7939f0ec85fbefc53da27
vanis <pavlik.vanis@email.cz>
2024-03-28 16:05:28 +0100

    Remove CSRF authorization

--- next-app/src/lib/api.ts
+++ next-app/src/lib/api.ts
@@ -1,40 +1,28 @@
-import { ApiResponse } from "../types/types";
 import { NextRequest, NextResponse } from "next/server";
 
-import { checkCsrfToken, checkSession } from "@/lib/auth";
+import { checkSession } from "@/lib/auth";
+import { ApiResponse } from "@/types/types";
 
 // Help methods for api routes
 
 // If SESSION_AUTHORIZE is set to true, it will skip the session authorization process. In production, it should be set to false.
 let SESSION_AUTHORIZE = false;
 
-// If CSRF_AUTHORIZE is set to true, it will skip the csrf authorization process. In production, it should be set to false.
-let SKIP_CSRF = false;
-
 if (process.env.NODE_ENV === "production") {
-  SKIP_CSRF = false;
   SESSION_AUTHORIZE = false;
 }
 
 /**
  * Authorize the user before accessing the protected route. Checks the session and CSRF token.
+ *
+ * Returns `401` if the session is invalid.
  */
 export const Authorize = async (
   req: NextRequest,
   res: NextResponse,
   next: (body: any) => Promise<ApiResponse>
 ) => {
-  const { csrfToken, ...body } = await req.json();
-
-  // Verify CSRF token
-  const { validCsrf, ...csrfMessage } = await checkCsrfToken(csrfToken, {
-    skip: SKIP_CSRF,
-  });
-
-  if (!validCsrf) {
-    console.log("message:  ", csrfMessage);
-    return NextResponse.json({ ...csrfMessage }, { status: 401 });
-  }
+  const { ...body } = await req.json();
 
   // Verify session
   const { validSession, ...sessionMessage } = await checkSession({
@@ -42,11 +30,10 @@ export const Authorize = async (
   });
 
   if (!validSession) {
+    // TODO: Add logger
     console.log("message:  ", sessionMessage);
     return NextResponse.json({ ...sessionMessage }, { status: 401 });
   }
-
-  console.log("Successfully authorized the request.");
 
   return next(body);
 };
