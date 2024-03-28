d5dec8231a02cd2517049bfa14a9955109a0ccfc
vanis <pavlik.vanis@email.cz>
2024-03-28 14:17:11 +0100

    Add session check for signout component in AuthLayout

--- next-app/src/app/(auth)/layout.tsx
+++ next-app/src/app/(auth)/layout.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import AuthHeader from "./_components/header";
 import AuthFooter from "./_components/footer";
+import { getServerSession } from "next-auth";
+import authOptions from "@/lib/authOptions";
+import Signout from "./_components/signout";
 
-const AuthLayout = ({ children }: React.PropsWithChildren) => {
+const AuthLayout = async ({ children }: React.PropsWithChildren) => {
+  const session = await getServerSession(authOptions);
+
   return (
     <>
       <AuthHeader className=" fixed w-screen top-0 left-1/2 -translate-x-1/2 z-[9999] max-w-6xl" />
       <main className=" min-h-screen max-w-6xl mx-auto flex flex-col justify-center items-center">
-        {children}
+        {session ? <Signout /> : children}
       </main>
       <AuthFooter className=" max-w-6xl mx-auto" />
     </>
