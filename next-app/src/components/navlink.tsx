0d7c0510cc6125dadf23a17b1df9875c8fc1e09f
vanis <pavlik.vanis@email.cz>
2024-03-28 15:59:51 +0100

    Create `NavLink` component

---
+++ next-app/src/components/navlink.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Typography } from "@/components/ui";
+
+export type NavLinkProps = PropsWithClassName & {
+  href: string;
+  title: string;
+};
+
+export const NavLink = ({ title, className, ...props }: NavLinkProps) => {
+  const pathname = usePathname();
+  const active = pathname.startsWith(props.href);
+  return (
+    <Typography
+      className={cn(
+        "hover:font-semibold transition-all w-20",
+        active && "font-semibold",
+        className
+      )}
+      as={Link}
+      {...props}
+    >
+      {title}
+    </Typography>
+  );
+};
