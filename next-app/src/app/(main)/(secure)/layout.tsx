1b20230fda0a0917ab01959de26a59950d83e11e
vanis <pavlik.vanis@email.cz>
2024-03-29 14:42:03 +0100

    Add session checking to layout

--- next-app/src/app/(main)/(secure)/layout.tsx
+++ next-app/src/app/(main)/(secure)/layout.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 import { SecureFooter, SecureHeader } from "./_components";
+import authOptions from "@/lib/authOptions";
+import { DEFAULT_UNAUTHORIZED_REDIRECT } from "@/config/routes";
 
-const SecureLayout = ({ children }: React.PropsWithChildren) => {
+const SecureLayout = async ({ children }: React.PropsWithChildren) => {
+  const session = await getServerSession(authOptions);
+
+  // Redirecting when there's no session
+  if (!session) {
+    redirect(DEFAULT_UNAUTHORIZED_REDIRECT);
+  }
+
   return (
-    <>
+    <div className="max-w-6xl mx-auto">
       <SecureHeader />
-      {children}
+      <main className=" min-h-screen flex flex-col justify-center items-center">
+        {children}
+      </main>
       <SecureFooter />
-    </>
+    </div>
   );
 };
 
