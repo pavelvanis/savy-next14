b604816653b952b13e34ea0eb5113aff218af082
vanis <pavlik.vanis@email.cz>
2024-03-29 14:42:13 +0100

    Update routes configuration

--- next-app/src/config/routes.ts
+++ next-app/src/config/routes.ts
@@ -4,18 +4,30 @@ import { NavLinkProps } from "@/components/navlink";
 export const NEXTAUTH_ROUTES: string[] = ["/dashboard", "/admin", "/settings"];
 
 // Default redirect after login
-export const DEFAULT_LOGIN_REDIRECT = "/";
+export const DEFAULT_LOGIN_REDIRECT = "/web";
+
+// Default redirect when user is not authorized
+export const DEFAULT_UNAUTHORIZED_REDIRECT = "/";
 
 // Links for haeder in auth layout
 export const authNavLinks: NavLinkProps[] = [
   { href: "/", title: "Home" },
   { href: "#", title: "Security" },
+  { href: "#", title: "Services" },
   { href: "#", title: "Privacy" },
 ];
 
-// Links for haeder in auth layout
+// Links for haeder in public layout
 export const mainNavLinks: NavLinkProps[] = [
-  { href: "/", title: "Integration" },
-  { href: "#", title: "Security" },
-  { href: "", title: "Services" },
+  { href: "/", title: "Home" },
+  { href: "", title: "Integration" },
+  { href: "/security", title: "Security" },
+  { href: "#", title: "Services" },
+];
+
+// Links for haeder in web layout
+export const webNavLinks: NavLinkProps[] = [
+  { href: "/web", title: "Balances" },
+  { href: "#", title: "Transactions" },
+  { href: "#", title: "Cards" },
 ];
