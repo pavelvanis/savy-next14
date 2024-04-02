902d24deda41eb93cfe2822c19c31ae394c865c5
vanis <pavlik.vanis@email.cz>
2024-04-02 14:37:56 +0200

    Create page for **/web/settings**

---
+++ next-app/src/app/(main)/(secure)/web/(routes)/settings/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+/**
+ * Page for allow user to change their settings
+ */
+const SettingsPage = () => {
+  return <div>SettingsPage</div>;
+};
+
+export default SettingsPage;
