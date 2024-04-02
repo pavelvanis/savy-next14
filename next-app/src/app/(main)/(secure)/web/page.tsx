bc611c41ba38cc44d4397fe7226d4eb1f7f291db
vanis <pavlik.vanis@email.cz>
2024-04-02 14:37:41 +0200

    Create page for **/web**

--- next-app/src/app/(main)/(secure)/web/page.tsx
+++ next-app/src/app/(main)/(secure)/web/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Link from "next/link";
+import { getAuthSession } from "@/lib/auth";
+import { Button, Typography } from "@/components/ui";
+import { addCredentialsLink } from "@/lib/tink/link";
+import { generateAuthorizationCode } from "@/lib/tink/actions";
+import { getCredentials } from "@/actions/server/credentials";
+import { GetCredentailsButton } from "@/actions/server/get";
 
-const WebPage = () => {
-  return <div>Users default home page</div>;
+const WebPage = async () => {
+  const { user } = await getAuthSession();
+
+  const auth_code = await generateAuthorizationCode(user.permanentUserId);
+
+  return (
+    <div>
+      <Typography variant="h3" className="font-bold text-center">
+        <span className="font-medium text-xl ">Welcome</span> {user.firstName}{" "}
+        {user.lastName}
+      </Typography>
+      <Typography variant="lead">{user.permanentUserId} </Typography>
+      {auth_code && (
+        <Link href={addCredentialsLink(auth_code.code, user.permanentUserId)}>
+          Add credentials
+        </Link>
+      )}
+      <GetCredentailsButton userId={user.permanentUserId} />
+    </div>
+  );
 };
 
 export default WebPage;
