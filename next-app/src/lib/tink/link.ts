74499872b42b2f9d5911c00d1f8762cf16cf2dce
vanis <pavlik.vanis@email.cz>
2024-04-02 10:00:43 +0200

    Refactor TinkLinkProps and remove unused code

--- next-app/src/lib/tink/link.ts
+++ next-app/src/lib/tink/link.ts
@@ -4,65 +4,18 @@ const TEST = process.env.NODE_ENV === "production" ? "false" : "true";
 const CLIENT_ID = process.env.TINK_CLIENT_ID;
 
 /**
- * TinkLinkProps is a type that represents the properties required to create a Tink Link.
- * It includes mandatory properties like endpoint, client_id, and redirect_uri, and optional properties like scope, market, locale, etc.
+ * Generates a URL for authenticating credentials in the Tink system.
+ * Read more about Tink Link initialization parameters: https://docs.tink.com/api/#initialization-parameters
  */
-type TinkLinkProps = {
-  endpoint: string;
-  client_id: string;
-  redirect_uri: string;
-} & Partial<{
-  scope: string;
-  market: string;
-  locale: string;
-  state: string;
-  session_id: string;
-  input_username: string;
-  input_provider: string;
-  iframe: string;
-  app_uri: string;
-  theme: "DARK" | "LIGHT";
-}>;
 
 /**
- * createTinkLink is a function that takes a TinkLinkProps object and returns a Tink Link.
+ * Generates a URL for authenticating credentials in the Tink system.
  */
-export const createTinkLink = ({
-  endpoint,
-  redirect_uri,
-  ...props
-}: TinkLinkProps) => {
-  const params = makeParams({
-    redirect_uri: encodeURIComponent(redirect_uri),
-    ...Object.fromEntries(
-      Object.entries(props).map(([key, value]) => [
-        key,
-        encodeURIComponent(value),
-      ])
-    ),
-  });
-
-  const link = `${TinkConfig.linkBaseUrl}${endpoint}?${params}`;
-
-  return link;
-};
-
-/**
- * makeParams is a helper function that takes an object of key-value pairs and returns a URL-encoded string.
- * It is used by createTinkLink to create the query string for the Tink Link.
- */
-const makeParams = (params: Record<string, string>) => {
-  return Object.entries(params)
-    .map(([key, value]) => `${key}=${value}`)
-    .join("&");
-};
-
 export const authenticateCredentialsLink = (
   authorizationCode: string,
   userId: string,
   credentialsId: string
 ) => {
-  // Read more about Tink Link initialization parameters: https://docs.tink.com/api/#initialization-parameters
   const params = [
     `client_id=${CLIENT_ID}`,
     `redirect_uri=${TinkConfig.callback}`,
@@ -80,6 +33,9 @@ export const authenticateCredentialsLink = (
   )}`;
 };
 
+/**
+ * Generates a URL for adding credentials in the Tink system.
+ */
 export const addCredentialsLink = (
   authorizationCode: string,
   userId: string
