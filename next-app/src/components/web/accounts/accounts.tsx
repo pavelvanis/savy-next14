fd7f7546201c348090b6d16e50f22d0d7cdd5510
vanis <pavlik.vanis@email.cz>
2024-04-04 16:01:14 +0200

    Refactor AccountsList component

--- next-app/src/components/web/accounts/accounts.tsx
+++ next-app/src/components/web/accounts/accounts.tsx
@@ -11,52 +11,19 @@ import { generateAuthorizationCode } from "@/lib/tink/actions";
 type AccountsListProps = TinkAccounts & {};
 
 const AccountsList: React.FC<AccountsListProps> = async ({
-  accounts,
-  ...props
+  accounts
 }) => {
-  const { user } = await getAuthSession();
-  const authorizationCode = await generateAuthorizationCode(
-    user.permanentUserId
-  );
   return (
-    <div className="mt-5 flex-1 w-full max-w-4xl">
-      <div className="flex flex-col gap-y-6 h-full w-full">
-        {/* Header */}
-        <div className="flex justify-between">
-          <Typography variant="h2" className="font-bold mb-2">
-            Accounts
-          </Typography>
-          <div>
-            <Button
-              variant="outlined"
-              size="sm"
-              className=" hover:bg-gray-200 transition-all duration-300"
-            >
-              {authorizationCode && (
-                <Link
-                  className=" flex items-center gap-2"
-                  href={addCredentialsLink(
-                    authorizationCode?.code,
-                    user.permanentUserId
-                  )}
-                >
-                  <PlusIcon className="w-5 h-5" />
-                  Add Account
-                </Link>
-              )}
-            </Button>
-          </div>
-        </div>
-        {/* Accounts */}
-        {accounts.length === 0 && (
-          <Typography variant="lead" className="font-semibold">
-            No accounts found
-          </Typography>
-        )}
-        {accounts.map((acc, i) => (
-          <AccountCard key={i} {...acc} />
-        ))}
-      </div>
+    <div className="flex flex-col gap-y-6 h-full w-full mt-3">
+      {/* Accounts */}
+      {accounts.length === 0 && (
+        <Typography variant="lead" className="font-semibold">
+          No accounts found
+        </Typography>
+      )}
+      {accounts.map((acc, i) => (
+        <AccountCard key={i} {...acc} />
+      ))}
     </div>
   );
 };
