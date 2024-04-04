b3930e5f7fcf6e83e0f744ce15e8addcbbe3952c
vanis <pavlik.vanis@email.cz>
2024-04-04 11:25:39 +0200

    Remove user name

--- next-app/src/components/layout/navbars/nav-user.tsx
+++ next-app/src/components/layout/navbars/nav-user.tsx
@@ -4,43 +4,30 @@ import { cn } from "@/lib/utils";
 import { Typography } from "@/components/ui";
 import { userNavLinks } from "@/config/routes";
 import { getAuthSession } from "@/lib/auth";
+import CustomLink from "@/components/link";
 
 type UserNavProps = PropsWithClassName & {};
 
 const UserNav: React.FC<UserNavProps> = async ({ className }) => {
-  const { user } = await getAuthSession();
   return (
-    <nav
-      className={cn("bg-red-0 h-14 flex items-center gap-x-10 px-5", className)}
-    >
-      {/* Name */}
-      <div className=" flex items-center gap-1.5">
-        <Typography className="text-lg font-semibold">
-          {user.firstName}
-        </Typography>
-        <Typography className="text-lg font-semibold">
-          {user.lastName}
-        </Typography>
-      </div>
-      {/* Actions */}
-
-      {/* Links */}
-      <ul className="flex items-center h-full w-full">
-        {userNavLinks.map((link, i) => (
-          <li
-            key={i}
-            className=" text-center h-full flex flex-grow items-center hover:bg-gray-600 hover:text-white transition-all"
-          >
-            <Link
-              className="flex-1 flex items-center justify-center h-full"
-              href={link.href}
-            >
-              {link.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className={cn(" bg-gray-200 shadow-md", className)}>
+      <nav className="bg-red-0 h-14 flex items-center gap-x-10 px-5 container">
+        {/* Links */}
+        <ul className="flex items-center h-full w-full">
+          {userNavLinks.map((link, i) => (
+            <li key={i} className=" user-nav-link ">
+              <CustomLink
+                className="flex-1 flex items-center justify-center h-full"
+                onActive="user-nav-link-active"
+                href={link.href}
+              >
+                {link.title}
+              </CustomLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
