ab7192bd20fbfb37b47ec35232729b4250a2fffc
vanis <pavlik.vanis@email.cz>
2024-04-04 21:44:05 +0200

    Show transactions data on page

--- next-app/src/app/(main)/(secure)/web/(routes)/transactions/page.tsx
+++ next-app/src/app/(main)/(secure)/web/(routes)/transactions/page.tsx
@@ -1,10 +1,19 @@
+import { getCredentials, getTransactions } from "@/actions/server/data";
+import { getAuthSession } from "@/lib/auth";
 import React from "react";
 
 /**
  * Page to show all transaction history
  */
-const Transactions = () => {
-  return <div>Transactions</div>;
+const Transactions = async () => {
+  const { user } = await getAuthSession();
+  const transactions = await getTransactions(user.permanentUserId);
+  console.log("Transactions: ", JSON.stringify(transactions, null, 2));
+  return (
+    <div>
+      <pre>{JSON.stringify(transactions, null, 2)}</pre>
+    </div>
+  );
 };
 
 export default Transactions;
