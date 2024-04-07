7ec667597f3f174318ee0901b11ecaf006b5304e
vanis <pavlik.vanis@email.cz>
2024-04-07 12:11:17 +0200

    Use menu for mobile view

--- next-app/src/components/layout/headers/header-web.tsx
+++ next-app/src/components/layout/headers/header-web.tsx
@@ -1,35 +1,26 @@
 import React from "react";
-import { MenuIcon } from "lucide-react";
 import { auth } from "@/lib/auth";
 import { cn } from "@/lib/utils";
-import { NavLink } from "@/components/navlink";
-import { mainNavLinks } from "@/config/routes";
-import { IconButton, Typography } from "@/components/ui";
+import { HeaderNavList } from "../navbars";
+import { Typography } from "@/components/ui";
 import LoginSignout from "@/components/auth/header-session";
+import { HeaderMenu } from "@/components/layout/menu";
 
-const PublicHeader = async ({ className }: PropsWithClassName) => {
+const HeaderWeb = async ({ className }: PropsWithClassName) => {
   const session = await auth();
   return (
     <div className=" bg-gradient-to-br from-white to-gray-100">
       <header
         className={cn(
-          " h-16 flex px-5 items-center justify-between gap-x-14 container",
+          " h-16 flex px-5 items-center justify-between container",
           className
         )}
       >
         {/* Logo */}
         <Typography className="whitespace-nowrap">Big logo here</Typography>
         {/* Nav */}
-        <div className="flex-1 flex items-center justify-center flex-wrap gap-x-4 gap-y-2">
-          <nav className=" hidden mx-auto">
-            <ul className="flex gap-x-4">
-              {mainNavLinks.map((link, i) => (
-                <li key={i}>
-                  <NavLink key={i} {...link} className="w-20" />
-                </li>
-              ))}
-            </ul>
-          </nav>
+        <div className="flex-1 flex-center flex-wrap gap-x-4 gap-y-2">
+          <HeaderNavList className="hidden md:flex flex-row gap-x-4 mx-auto" />
           {/* Login & Register | Signout */}
           {session && (
             <Typography className="ms-auto whitespace-nowrap text-xl font-semibold">
@@ -37,19 +28,13 @@ const PublicHeader = async ({ className }: PropsWithClassName) => {
             </Typography>
           )}
           {/* Menu */}
-          <IconButton
-            variant="text"
-            className="user-nav-link max-w-8 max-h-8 sm:hidden "
-          >
-            {/* <AlignRightIcon className="icon" /> */}
-            <MenuIcon className="icon" />
-          </IconButton>
+          <HeaderMenu loginState={!!session} />
 
-          <LoginSignout className="hidden sm:block" state={!!session} />
+          <LoginSignout className="hidden md:block" state={!!session} />
         </div>
       </header>
     </div>
   );
 };
 
-export default PublicHeader;
+export default HeaderWeb;
