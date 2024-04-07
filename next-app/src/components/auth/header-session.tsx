af73cf432c99bfc81e55b6c58898493247d63886
vanis <pavlik.vanis@email.cz>
2024-04-07 15:25:13 +0200

    Update header session component to use logout button component

--- next-app/src/components/auth/header-session.tsx
+++ next-app/src/components/auth/header-session.tsx
@@ -2,11 +2,15 @@
 
 import React from "react";
 import Link from "next/link";
-import { KeyRoundIcon, LogOutIcon, UsersRoundIcon } from "lucide-react";
-import Signout from "@/components/auth/signout";
-import { Button } from "@/components/ui";
+import {
+  BookOpenTextIcon,
+  KeyRoundIcon,
+  LogOutIcon,
+} from "lucide-react";
 import { useSession } from "next-auth/react";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui";
+import { LogoutButton } from "./logout-button";
 
 /**
  * If user is authenticated, show signout button, else show login and register button
@@ -37,23 +41,29 @@ export default LoginSignout;
 
 const LoginRegister = ({ className }: PropsWithClassName) => (
   <div className={cn("flex gap-x-3", className)}>
-    <Button variant="outlined" size="sm" className="flex gap-x-2 items-center">
-      <KeyRoundIcon className="w-4 h-4" />
-      <Link href="/login">Log in</Link>
+    <Button variant="outlined" size="sm" className="btn-hover">
+      <Link href="/login" className="flex-center gap-x-2">
+        <KeyRoundIcon className="size-4" />
+        Log in
+      </Link>
     </Button>
-    <Button variant="outlined" size="sm" className="flex gap-x-2 items-center">
-      <UsersRoundIcon className="w-4 h-4" />
-      <Link href="/register">Register</Link>
+    <Button variant="outlined" size="sm" className="btn-hover">
+      <Link href="/register" className="flex-center gap-x-2">
+        <BookOpenTextIcon className="size-4" />
+        Register
+      </Link>
     </Button>
   </div>
 );
 
 const LogOut = ({ className }: PropsWithClassName) => (
-  <Signout
-    variant="text"
-    size="sm"
-    className={cn("flex gap-x-2 items-center", className)}
-  >
-    <LogOutIcon className="w-4 h-4" />
-  </Signout>
+  <LogoutButton>
+    <Button
+      variant="text"
+      size="sm"
+      className={cn("flex gap-x-2 items-center btn-hover", className)}
+    >
+      <LogOutIcon className="size-4" />
+    </Button>
+  </LogoutButton>
 );
