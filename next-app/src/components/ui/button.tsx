76815dad00bbd377a44c55783b3af908015c8aaa
vanis <pavlik.vanis@email.cz>
2024-04-08 15:59:22 +0200

    Export props of component

--- next-app/src/components/ui/button.tsx
+++ next-app/src/components/ui/button.tsx
@@ -3,3 +3,5 @@
 import { Button, IconButton } from "@material-tailwind/react";
 
 export { Button, IconButton };
+
+export { type ButtonProps } from "@material-tailwind/react";
--- next-app/src/components/ui/typography.tsx
+++ next-app/src/components/ui/typography.tsx
@@ -8,7 +8,14 @@ const Typography = React.forwardRef<
   React.ElementRef<typeof MTTypography>,
   React.ComponentPropsWithoutRef<typeof MTTypography>
 >(({ className, ...props }, ref) => {
-  return <MTTypography className={cn(" font-red", className)} {...props} ref={ref} />;
+  return (
+    <MTTypography className={cn(" font-red", className)} {...props} ref={ref} />
+  );
 });
 
 export { Typography };
+
+export {
+  type TypographyProps,
+  type TypographyStylesType,
+} from "@material-tailwind/react";
