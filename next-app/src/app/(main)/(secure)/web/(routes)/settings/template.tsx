78e339968b0b13db0dd1134fedb1006cfffa4c48
vanis <pavlik.vanis@email.cz>
2024-04-08 16:00:23 +0200

    Make responsive settings template

--- next-app/src/app/(main)/(secure)/web/(routes)/settings/template.tsx
+++ next-app/src/app/(main)/(secure)/web/(routes)/settings/template.tsx
@@ -1,48 +1,60 @@
 import React from "react";
 import { Settings2Icon } from "lucide-react";
-import { Card, List, ListDivider, ListItem, Typography } from "@/components/ui";
+import {
+  Page,
+  PageNavbar,
+  PageNavbarProps,
+} from "@/components/layout/page-components";
+import { cn } from "@/lib/utils";
 import CustomLink from "@/components/link";
 import { settingsLinks } from "@/config/routes";
+import { Card, List, ListDivider, ListItem } from "@/components/ui";
+import { SettingsMenu } from "@/components/layout/menu/menu-settings";
 
-const SettingsTemplate = ({ children }: React.PropsWithChildren) => {
+interface TemplateSettingsProps extends React.PropsWithChildren {}
+
+const TemplateSettings: React.FC<TemplateSettingsProps> = ({ children }) => {
+  const settingsNavbarProps: PageNavbarProps = {
+    title: {
+      children: "Settings",
+      icon: Settings2Icon,
+    },
+    button: false,
+  };
   return (
-    <div className="page-container flex flex-col">
-      {/* Page header */}
-      <div className="page-header">
-        <div className="flex items-center gap-x-4">
-          <Typography variant="h2" className="font-bold">
-            Settings
-          </Typography>
-          <Settings2Icon className="size-8" />
-          {/* <WrenchIcon className="size-8" /> */}
-          {/* <UserCogIcon className="size-8" /> */}
-          {/* <UserCog2Icon className="size-8" /> */}
-        </div>
-      </div>
+    <Page>
+      <PageNavbar {...settingsNavbarProps}>
+        <SettingsMenu className="md:hidden" />
+      </PageNavbar>
       <div className="flex flex-1 w-full h-full gap-x-10">
-        <div>
-          <Card className="p-0 w-52">
-            <List>
-              {settingsLinks.map((list) => (
-                <>
-                  {list.map(({ ...props }) => (
-                    <CustomLink
-                      onActive="bg-blue-gray-50 bg-opacity-40 rounded-lg"
-                      href={props.href}
-                    >
-                      <ListItem key={props.href}>{props.title}</ListItem>
-                    </CustomLink>
-                  ))}
-                  <ListDivider />
-                </>
-              ))}
-            </List>
-          </Card>
-        </div>
-        <section className=" flex-1">{children}</section>
+        <SettingsNavbar className="hidden md:block" />
+        <section className="mt-3 md:mt-0 flex-1">{children}</section>
       </div>
-    </div>
+    </Page>
   );
 };
 
-export default SettingsTemplate;
+export default TemplateSettings;
+
+const SettingsNavbar = ({ className }: PropsWithClassName) => (
+  <div className={cn(className)}>
+    <Card className="p-0 w-52">
+      <List>
+        {settingsLinks.map((list, i) => (
+          <>
+            {list.map(({ ...props }, i) => (
+              <CustomLink
+                key={i}
+                onActive="bg-blue-gray-50 bg-opacity-40 rounded-lg"
+                href={props.href}
+              >
+                <ListItem key={props.href}>{props.title}</ListItem>
+              </CustomLink>
+            ))}
+            {settingsLinks.length - 1 !== i && <ListDivider />}
+          </>
+        ))}
+      </List>
+    </Card>
+  </div>
+);
