7f4bcef94cbc9c75d2f84c1b092d908d0157bfc4
vanis <pavlik.vanis@email.cz>
2024-04-08 19:24:49 +0200

    Add Toaster component for displaying toasts in RootLayout

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter, Red_Hat_Display } from "next/font/google";
 
+import { Toaster } from "sonner";
 import { ThemeProvider, SessionProvider } from "@/components/providers";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,7 +24,10 @@ export default function RootLayout({
     <html lang="en">
       <ThemeProvider>
         <SessionProvider>
-          <body className={font.className}>{children}</body>
+          <body className={font.className}>
+            <Toaster richColors closeButton visibleToasts={5} />
+            {children}
+          </body>
         </SessionProvider>
       </ThemeProvider>
     </html>
