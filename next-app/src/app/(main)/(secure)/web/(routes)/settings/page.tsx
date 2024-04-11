368d501c0a4ba95ddc4f7f1728c1e386ef504a58
vanis <pavlik.vanis@email.cz>
2024-04-11 10:42:55 +0200

    Refactor settings page to include account information and settings row component

--- next-app/src/app/(main)/(secure)/web/(routes)/settings/page.tsx
+++ next-app/src/app/(main)/(secure)/web/(routes)/settings/page.tsx
@@ -1,10 +1,60 @@
 import React from "react";
+import {
+  Page,
+  PageNavbar,
+  PageNavbarProps,
+} from "@/components/layout/page-components";
+import { Typography } from "@/components/ui";
+import { getAuthSession } from "@/lib/auth";
+import { HelpTooltip } from "@/components";
+
+const settingsNavbarProps: PageNavbarProps = {
+  title: {
+    children: "Account",
+    variant: "h3",
+    className: "font-semibold",
+  },
+  button: false,
+};
 
 /**
  * This page allows the user to change the settings
  */
 const SettingsPage = async () => {
-  return <div>Default account page</div>;
+  const { user } = await getAuthSession();
+  return (
+    <Page>
+      <PageNavbar {...settingsNavbarProps} />
+      <section className="page-body">
+        <SettingsRow
+          label="name:"
+          value={`${user.firstName} ${user.lastName}`}
+        />
+        <SettingsRow label="email:" value={user.email} />
+        <SettingsRow
+          label="Generated user id:"
+          value={user.permanentUserId}
+          children={
+            <HelpTooltip content="This is permanent user id generated after you have registered and is used to accessing your data in banks." />
+          }
+        />
+      </section>
+    </Page>
+  );
 };
 
 export default SettingsPage;
+
+const SettingsRow: React.FC<{
+  label: React.ReactNode;
+  value: React.ReactNode;
+  children?: React.ReactNode;
+}> = ({ label, value, children }) => (
+  <Typography as="div" className="flex items-center gap-x-2">
+    {label}
+    <Typography as="span" className="font-normal text-lg">
+      {value}
+    </Typography>
+    {children}
+  </Typography>
+);
