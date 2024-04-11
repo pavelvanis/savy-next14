94b9854a51e73ef654d9261ca66dcf316fe22e5f
vanis <pavlik.vanis@email.cz>
2024-04-11 12:42:44 +0200

    Refactor AccountsList component to include FilterSortTable and accountColumns

--- next-app/src/components/web/accounts/accounts.tsx
+++ next-app/src/components/web/accounts/accounts.tsx
@@ -2,26 +2,70 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import AccountCard from "./account-card";
 import { Typography } from "@/components/ui";
-import { TinkAccounts } from "@/types/types";
+import { TinkAccount, TinkAccounts } from "@/types/types";
+import { FilterSortTable } from "@/components/sort-table";
+import { ColumnDef } from "@tanstack/react-table";
 
 type AccountsListProps = TinkAccounts & PropsWithClassName & {};
+
+const accountColumns: ColumnDef<TinkAccount>[] = [
+  {
+    accessorKey: "id",
+    header: "Id",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "type",
+    header: "Type",
+  },
+  {
+    accessorKey: "currencyCode",
+    header: "Currency",
+  },
+  {
+    accessorKey: "balance",
+    header: "Balance",
+  },
+  {
+    accessorKey: "accountNumber",
+    header: "Account number",
+  },
+  {
+    accessorKey: "credentialsId",
+    header: "Credentials id",
+  },
+  {
+    accessorKey: "userId",
+    header: "User id",
+  },
+  {
+    accessorKey: "updated",
+    header: "Updated",
+  },
+];
 
 const AccountsList: React.FC<AccountsListProps> = async ({
   className,
   accounts,
 }) => {
+  console.log(accounts[0]);
   return (
     <section className={cn("list-col", className)}>
       {/* No accounts */}
       {accounts.length === 0 && (
-        <Typography variant="lead" className="font-semibold">
+        <Typography variant="lead" className="font-semibold text-lg">
           You don&apos;t have any accounts connected yet
         </Typography>
       )}
-      {/* Account list */}
-      {accounts.map((acc, i) => (
-        <AccountCard key={i} {...acc} />
-      ))}
+      {/* Table of accounts */}
+      <FilterSortTable
+        Component={AccountCard}
+        columns={accountColumns}
+        data={accounts}
+      />
     </section>
   );
 };
