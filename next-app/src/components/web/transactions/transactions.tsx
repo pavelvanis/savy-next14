409d2da7755270d131c532c93173acfda79e1f2c
vanis <pavlik.vanis@email.cz>
2024-04-13 19:02:43 +0200

    Refactor TransactionList component to use React Table for sorting and pagination

--- next-app/src/components/web/transactions/transactions.tsx
+++ next-app/src/components/web/transactions/transactions.tsx
@@ -1,26 +1,83 @@
+"use client";
+
 import React from "react";
-import { cn } from "@/lib/utils";
+import { cn, getAmount } from "@/lib/utils";
 import { Typography } from "@/components/ui";
-import { TinkTransactions } from "@/types/tink";
+import { TinkTransaction, TinkTransactions } from "@/types/tink";
+import {
+  ReactTable,
+  ReactTableBody,
+  ReactTablePagination,
+} from "@/components/react-table";
+import {
+  ColumnDef,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import SortableTransactionsHeader from "./transactions-sortable";
 import TransactionCard from "./transaction-card";
 
 type TransactionListProps = PropsWithClassName & TinkTransactions & {};
+
+const transactionColumns: ColumnDef<TinkTransaction>[] = [
+  {
+    accessorKey: "id",
+    header: "Id",
+  },
+  {
+    accessorKey: "date",
+    accessorFn: (data) => data.dates.booked,
+  },
+  {
+    accessorKey: "amount",
+    accessorFn: (data) =>
+      getAmount(data.amount.value.scale, data.amount.value.unscaledValue),
+  },
+  {
+    accessorKey: "status",
+    accessorFn: (data) => data.status,
+  },
+  {
+    accessorKey: "description",
+    accessorFn: (data) => data.descriptions.display,
+  },
+  {
+    accessorKey: "currencyCode",
+    accessorFn: (data) => data.amount.currencyCode,
+  },
+  {
+    accessorKey: "accountId",
+    accessorFn: (data) => data.accountId,
+  },
+];
 
 const TransactionList: React.FC<TransactionListProps> = ({
   className,
   transactions,
 }) => {
+  const table = useReactTable({
+    data: transactions,
+    columns: transactionColumns,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+  });
   return (
     <section className={cn("list-col", className)}>
       {/* Accounts */}
-      {transactions.length === 0 && (
+      {transactions.length === 0 ? (
         <Typography variant="lead" className="font-semibold">
           No accounts found
         </Typography>
+      ) : (
+        <ReactTable Component={TransactionCard} table={table}>
+          <SortableTransactionsHeader />
+          <ReactTableBody />
+          <ReactTablePagination />
+        </ReactTable>
       )}
-      {transactions.map((acc, i) => (
-        <TransactionCard key={i} {...acc} />
-      ))}
     </section>
   );
 };
