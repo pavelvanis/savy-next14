c66fe2d751559067aa577decdc1a836637780c9a
vanis <pavlik.vanis@email.cz>
2024-04-16 10:27:20 +0200

    Change name from `transactions-chart` to `transactions-navbar`

--- next-app/src/components/web/transactions/transactions-navbar.tsx
+++ next-app/src/components/web/transactions/transactions-navbar.tsx
@@ -1,21 +1,29 @@
-import { getBalance, getExpensesAmount, getIncomeAmount } from "@/lib/utils";
+import {
+  cn,
+  getBalance,
+  getExpensesAmount,
+  getIncomeAmount,
+} from "@/lib/utils";
 import React from "react";
 import { TinkTransaction } from "@/types/tink";
 import { BalanceText } from "../balance";
 
 interface TransacationsMonthNavbarProps extends PropsWithClassName {
-  date: string;
   transactions: TinkTransaction[];
 }
 
 const TransacationsMonthNavbar: React.FC<TransacationsMonthNavbarProps> = ({
-  date,
   transactions,
   className,
 }) => {
   const currencyCode = transactions[0].amount.currencyCode;
   return (
-    <div className=" bg-gradient-to-br from-gray-300/30 to-gray-300/60 border-2 border-gray-400/30 rounded-lg p-2 mt-2 flex gap-2 flex-wrap">
+    <div
+      className={cn(
+        " bg-gradient-to-br from-gray-300/30 to-gray-300/60 border-2 border-gray-400/30 rounded-lg p-2 mt-2 flex gap-2 flex-wrap",
+        className
+      )}
+    >
       <div className=" flex-1 flex-center gap-x-2 text-center">
         Income:{" "}
         <BalanceText
