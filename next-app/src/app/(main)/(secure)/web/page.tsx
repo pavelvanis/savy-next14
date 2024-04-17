4ecf7f473832c114739a1de9f66bfab267843289
vanis <pavlik.vanis@email.cz>
2024-04-17 19:04:20 +0200

    Show accounts overview

--- next-app/src/app/(main)/(secure)/web/page.tsx
+++ next-app/src/app/(main)/(secure)/web/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { getAuthSession } from "@/lib/auth";
 import { Typography } from "@/components/ui";
+import { AccountsOverview } from "@/components/web/overviews";
+import { Page } from "@/components/layout/page-components";
 
 const WebPage = async () => {
   const { user } = await getAuthSession();
 
   return (
-    <div>
+    <Page>
+      <AccountsOverview user={user} />
       <Typography variant="h3" className="font-bold text-center">
         <span className="font-medium text-xl ">Welcome</span> {user.firstName}{" "}
         {user.lastName}
@@ -14,7 +17,7 @@ const WebPage = async () => {
       <Typography variant="lead" className=" text-base mt-3 text-center">
         Here will be some overviews of your accounts and transactions.
       </Typography>
-    </div>
+    </Page>
   );
 };
 
