37b5be81f480ba22adfa461525a3f1497008b651
vanis <pavlik.vanis@email.cz>
2024-04-17 11:33:15 +0200

    Create component to show balance trend line

--- next-app/src/components/web/balance.tsx
+++ next-app/src/components/web/balance.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Typography } from "../ui";
 import { cn, getFormatedAmount } from "@/lib/utils";
+import { TrendingDown, TrendingUp } from "lucide-react";
+import { Typography } from "../ui";
 
 interface BalanceTextProps extends PropsWithClassName {
   amount: number;
@@ -24,6 +25,30 @@ export const BalanceText: React.FC<BalanceTextProps> = ({
       )}
     >
       {getFormatedAmount(amount, currencyCode)}
+    </Typography>
+  );
+};
+
+interface BalanceLineProps extends PropsWithClassName {
+  amount: number;
+}
+
+export const BalanceLine: React.FC<BalanceLineProps> = ({ amount }) => {
+  const isPositive = amount >= 0;
+  return (
+    <Typography
+      as="span"
+      className={cn(
+        "font-normal flex-center gap-x-1",
+        isPositive ? "text-green-800" : "text-red-800"
+      )}
+    >
+      {isPositive ? (
+        <TrendingUp className={cn("size-5")} strokeWidth={1.3} />
+      ) : (
+        <TrendingDown className={cn("size-5")} strokeWidth={1.3} />
+      )}
+      {amount}%
     </Typography>
   );
 };
