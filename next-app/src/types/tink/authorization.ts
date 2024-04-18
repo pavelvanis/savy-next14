d98a20e95d1144fc8a1ed31c067370af75393491
vanis <pavlik.vanis@email.cz>
2024-04-18 16:18:44 +0200

    Add TinkCallbackError type to authorization.ts

--- next-app/src/types/tink/authorization.ts
+++ next-app/src/types/tink/authorization.ts
@@ -14,3 +14,11 @@ export type TinkPermanentUser = {
   user_id: string;
   external_user_id: string;
 };
+
+export type TinkCallbackError =
+  | "USER_CANCELLED"
+  | "BAD_REQUEST"
+  | "INVALID_STATE"
+  | "AUTHENTICATION_ERROR"
+  | "TEMPORARY_ERROR"
+  | "INTERNAL_ERROR"
