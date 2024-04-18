9cc2f87b8691174bd76f23d89b92307e43e2e1e6
vanis <pavlik.vanis@email.cz>
2024-04-18 12:58:16 +0200

    Change the image for Chart Line with transactions

--- next-app/src/app/(main)/(secure)/web/page.tsx
+++ next-app/src/app/(main)/(secure)/web/page.tsx
@@ -5,13 +5,19 @@ import { Typography } from "@/components/ui";
 import { Page } from "@/components/layout/page-components";
 import { AccountsOverview } from "@/components/web/overviews";
 import TransactionsOverview from "@/components/web/overviews/overviews-transactions";
+import { TransactionsBalancesChart } from "@/components/web/transactions/transactins-chart";
+import { getTransactions } from "@/actions/server/data";
 
 const WebPage = async () => {
   const { user } = await getAuthSession();
 
+  const transactinsResponse = await getTransactions(user.permanentUserId);
+
+  const transactions = transactinsResponse.data?.transactions;
+
   return (
     <Page>
-      <div className="flex gap-4 flex-col">
+      <div className="flex gap-5 flex-col">
         <div className="mb-4">
           <Typography variant="h3" className="font-bold text-center">
             <span className="font-medium text-xl ">Welcome</span>{" "}
@@ -22,16 +28,12 @@ const WebPage = async () => {
           </Typography>
         </div>
         <AccountsOverview user={user} />
-        <div className="grid  sm:grid-cols-2">
+        <div className="grid  md:grid-cols-2">
           <TransactionsOverview user={user} />
           <div className="flex-center">
-            <Image
-              alt="Personal finance"
-              src="/statistics_01.svg"
-              width={500}
-              height={500}
-              className=" object-cover h-60 w-60"
-            />
+            {transactions && (
+              <TransactionsBalancesChart transactions={transactions} />
+            )}
           </div>
         </div>
       </div>
