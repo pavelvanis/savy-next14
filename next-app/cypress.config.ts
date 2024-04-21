68ac2d80677ed670c474b90329e69e354ae1a3f6
vanis <pavlik.vanis@email.cz>
2024-04-21 19:43:10 +0200

    Update Cypress configuration and environment variables

--- next-app/cypress.config.ts
+++ next-app/cypress.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {},
+    baseUrl: "http://localhost:3000",
+    chromeWebSecurity: false,
+    env: {
+      user: {
+        firstName: "John",
+        lastName: "Doe",
+        email: "test@test.com",
+        password: "password",
+        confirmPassword: "password",
+      },
     },
   },
 });
