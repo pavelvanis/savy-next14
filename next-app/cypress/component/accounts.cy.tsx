9b5aa9a6e43c653d551976b0e8cf706eaf838845
vanis <pavlik.vanis@email.cz>
2024-04-21 22:04:38 +0200

    Add Cypress tests for rendering account cards with TestWrapper component

--- next-app/cypress/component/accounts.cy.tsx
+++ next-app/cypress/component/accounts.cy.tsx
@@ -1,14 +1,30 @@
-import "@/styles/globals.css";
-
-import { TinkAccount } from "@/types/tink";
+import { TinkAccount, TinkAccounts } from "@/types/tink";
 import AccountCard from "@/components/web/accounts/account-card";
+import AccountsList from "@/components/web/accounts/accounts";
+import { TestWrapper } from "../test-wrapper";
 
 describe("Accounts", () => {
+  // Only card rendering is tested here
   it("Accounts card rendering", () => {
-    cy.fixture("accounts.json").then((accounts) => {
+    cy.fixture("accounts.json").then(({ accounts }: TinkAccounts) => {
       accounts.forEach((account: TinkAccount) => {
-        cy.mount(<AccountCard {...account} />);
+        cy.mount(
+          <TestWrapper>
+            <AccountCard {...account} />
+          </TestWrapper>
+        );
       });
+    });
+  });
+
+  // All card in react-table sortable list rendering is tested here
+  it("Accounts sortable list rendering", () => {
+    cy.fixture("accounts.json").then((accounts: TinkAccounts) => {
+      cy.mount(
+        <TestWrapper>
+          <AccountsList {...accounts} />
+        </TestWrapper>
+      );
     });
   });
 });
