41bfe4379813124e2eb392653a64135056c9429d
vanis <pavlik.vanis@email.cz>
2024-04-21 11:42:42 +0200

    Create first Success and Failure tests

---
+++ next-app/cypress/e2e/firstTest.cy.ts
@@ -0,0 +1,9 @@
+describe("My First Test", () => {
+  it("Failure test!", () => {
+    expect(true).to.equal(false);
+  });
+
+  it("Success test!", () => {
+    expect(true).to.equal(true);
+  });
+});
