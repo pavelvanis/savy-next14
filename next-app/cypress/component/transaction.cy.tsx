ec296b2c1ab6eb1596544892cad6bf8d43bc23ce
vanis <pavlik.vanis@email.cz>
2024-04-21 22:23:04 +0200

    Update Cypress tests for rendering transaction cards and account cards with TestWrapper component

--- next-app/cypress/component/transaction.cy.tsx
+++ next-app/cypress/component/transaction.cy.tsx
@@ -1,14 +1,39 @@
-import "@/styles/globals.css";
-
-import { TinkTransaction } from "@/types/tink";
+import { TinkTransaction, TinkTransactions } from "@/types/tink";
 import TransactionCard from "@/components/web/transactions/transaction-card";
+import { TestWrapper } from "../test-wrapper";
+import TransactionList from "@/components/web/transactions/transactions";
+import { groupByMonth } from "@/lib/utils";
 
 describe("Transactions", () => {
-  it("Transaction card rendering", () => {
-    cy.fixture("transactions.json").then((transactions) => {
-      transactions.forEach((transaction: TinkTransaction) => {
-        cy.mount(<TransactionCard {...transaction} />);
-      });
+  let transactions: TinkTransactions;
+
+  // Load transactions fixture
+  before(() => {
+    cy.fixture("transactions.json").then(({ ...loaded }: TinkTransactions) => {
+      transactions = loaded;
     });
+  });
+
+  // Only card rendering is tested here
+  it("Transaction card rendering", () => {
+    transactions.transactions.forEach((transaction: TinkTransaction) => {
+      cy.mount(
+        <TestWrapper>
+          <TransactionCard {...transaction} />
+        </TestWrapper>
+      );
+    });
+  });
+
+  // All card in react-table sortable list rendering is tested here
+  it("Transactions sortable list rendering", () => {
+    const transactionsByMonth = groupByMonth(transactions.transactions);
+    cy.mount(
+      <TestWrapper>
+        {Object.entries(transactionsByMonth).map(([date]) => (
+          <TransactionList key={date} {...transactions} date={date} />
+        ))}
+      </TestWrapper>
+    );
   });
 });
