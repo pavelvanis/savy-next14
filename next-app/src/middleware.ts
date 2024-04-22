44545b1b693ad4325cf5dd405403ffc3d99882b7
vanis <pavlik.vanis@email.cz>
2024-04-22 08:11:17 +0200

    Add @edge-csrf/nextjs package and middleware for CSRF protection

---
+++ next-app/src/middleware.ts
@@ -0,0 +1,12 @@
+// middleware.ts
+
+import { createCsrfMiddleware } from '@edge-csrf/nextjs';
+
+// initalize csrf protection middleware
+const csrfMiddleware = createCsrfMiddleware({
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+});
+
+export const middleware = csrfMiddleware;
