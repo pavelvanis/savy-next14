750d33e7eef84e0a2d5985e671b5cba2bc70c8a4
vanis <pavlik.vanis@email.cz>
2024-04-22 16:25:43 +0200

    Remove privacy and security pages

--- next-app/src/app/(main)/(public)/(routes)/privacy/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PrivacyPage = () => {
-  return <div>Page with terms and consents</div>;
-};
-
-export default PrivacyPage;
--- next-app/src/app/(main)/(public)/(routes)/security/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SecurityPage = () => {
-  return (
-    <div>SecurityPage</div>
-  )
-}
-
-export default SecurityPage
